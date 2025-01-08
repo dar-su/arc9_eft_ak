4cddf65509f121898ff938b8fa0e3fd8ecefc8d6
Darsu <95997959+dar-su@users.noreply.github.com>
2025-01-09 04:44:48 +0700

    Update arc9_eft_pp1901.lua

--- lua/weapons/arc9_eft_pp1901.lua
+++ lua/weapons/arc9_eft_pp1901.lua
@@ -52,7 +52,7 @@ SWEP.CustomizeSnapshotFOV = 90
 SWEP.CustomizeRotateAnchor = Vector(19, -4.28, -5.23)
 
 -- this thing can one hand sprint
-local handupang, handuppos, handupholdtype = Angle(-2, 40, -7), Vector(1, -5, -6), "normal"
+local handupang, handuppos, handupholdtype = Angle(-2, 40, -7), Vector(0, -4, -10), "normal"
 SWEP.OneHandedSprint = true
 SWEP.OneHandedSprintHook = function(self, old) if self:GetValue("HasStock") then return false end end
 SWEP.SprintAngHook = function(self, old) if !self:GetValue("HasStock") then return handupang end end
